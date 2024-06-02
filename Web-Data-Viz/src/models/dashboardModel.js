1091b90de7d3cdecbfa8c84d98b7ff54ee9bbff7
Lucas Canuto <lucas.canuto@sptech.school>
2024-06-02 17:54:28 -0300

    Aplicação da parte de historico na dashboard linkado com o banco de dados

--- Web-Data-Viz/src/models/dashboardModel.js
+++ Web-Data-Viz/src/models/dashboardModel.js
@@ -2,10 +2,17 @@ var database = require("../database/config");
 
 function historico() {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
-    var instrucaoSql = ``;
+    var instrucaoSql = 
+                    `SELECT d.temperatura, d.umidade, date_format(d.dtRegistrada, '%d/%m/%y %h:%m') as DataAtual, s.idSetor, f.nome as NomeFazenda FROM dados as d JOIN setor as s 	
+                        ON d.fkSetor = s.idSetor
+                            JOIN fazenda as f
+                                ON s.fkFazenda = f.idFazenda
+                                    WHERE idFazenda = 1
+                                        ORDER BY dtRegistrada`;
+
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 module.exports = {
-   historico,
+    historico,
 }
