9563d68ee52e18dedc38ecac1f91c39fd525c4fe
CaioAcayabaFurtado <caio.furtado@sptech.school>
2024-06-06 15:25:24 -0300

    Card dinamicos prontos

--- Web-Data-Viz/src/routes/dashboard.js
+++ Web-Data-Viz/src/routes/dashboard.js
@@ -6,4 +6,17 @@ var dashboardController = require("../controllers/dashboardController.js");
 router.get("/historico", function (req, res) {
     dashboardController.historico(req, res);
 });
+
+router.get("/countCritico", function (req, res) {
+    dashboardController.countCritico(req, res);
+});
+
+router.get("/tempCritico", function (req, res) {
+    dashboardController.tempCritico(req, res);
+});
+
+router.get("/umidCritico", function (req, res) {
+    dashboardController.umidCritico(req, res);
+});
+
 module.exports = router;
